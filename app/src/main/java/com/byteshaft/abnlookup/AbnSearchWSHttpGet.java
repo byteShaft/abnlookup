90fd7ba5e67bd65a1d74bc0d5951139751eac9dd
s9iper1 <s9iper1@gmail.com>
2018-05-23 13:59:41 +0500

    code setup

--- app/src/main/java/com/byteshaft/abnlookup/AbnSearchWSHttpGet.java
+++ app/src/main/java/com/byteshaft/abnlookup/AbnSearchWSHttpGet.java
@@ -24,7 +24,7 @@ public class AbnSearchWSHttpGet
 			String guid = "<insert your GUID here>";
 			String abn = "69 410 335 356";
 
-			ditr.abn.example.AbnSearchResult result = searchByABN(guid, abn, false);
+			AbnSearchResult result = searchByABN(guid, abn, false);
 
 			if (!result.isException())
 				System.out.println("ABN search for ABN [" + abn + "] returned business name [" + result.getOrganisationName() + "]");
@@ -38,10 +38,10 @@ public class AbnSearchWSHttpGet
 		}
 	}
 
-	public static ditr.abn.example.AbnSearchResult searchByABN(String guid, String abn, boolean includeHistorical) throws URISyntaxException, IOException,
+	public static AbnSearchResult searchByABN(String guid, String abn, boolean includeHistorical) throws URISyntaxException, IOException,
 			SAXException, ParserConfigurationException, FactoryConfigurationError
 	{
-		ditr.abn.example.AbnSearchResult results = null;
+		AbnSearchResult results = null;
 
 		String params = "";
 
@@ -53,10 +53,10 @@ public class AbnSearchWSHttpGet
 		return results;
 	}
 
-	public static ditr.abn.example.AbnSearchResult searchByACN(String guid, String acn, boolean includeHistorical) throws URISyntaxException, IOException,
+	public static AbnSearchResult searchByACN(String guid, String acn, boolean includeHistorical) throws URISyntaxException, IOException,
 			SAXException, ParserConfigurationException, FactoryConfigurationError
 	{
-		ditr.abn.example.AbnSearchResult results = null;
+		AbnSearchResult results = null;
 
 		String params = "";
 
@@ -68,10 +68,10 @@ public class AbnSearchWSHttpGet
 		return results;
 	}
 
-	public static ditr.abn.example.AbnSearchResult searchByABNv200506(String guid, String abn, boolean includeHistorical) throws URISyntaxException, IOException,
+	public static AbnSearchResult searchByABNv200506(String guid, String abn, boolean includeHistorical) throws URISyntaxException, IOException,
 			SAXException, ParserConfigurationException, FactoryConfigurationError
 	{
-		ditr.abn.example.AbnSearchResult results = null;
+		AbnSearchResult results = null;
 
 		String params = "";
 
@@ -83,10 +83,10 @@ public class AbnSearchWSHttpGet
 		return results;
 	}
 
-	public static ditr.abn.example.AbnSearchResult searchByACNv200506(String guid, String acn, boolean includeHistorical) throws URISyntaxException, IOException,
+	public static AbnSearchResult searchByACNv200506(String guid, String acn, boolean includeHistorical) throws URISyntaxException, IOException,
 			SAXException, ParserConfigurationException, FactoryConfigurationError
 	{
-		ditr.abn.example.AbnSearchResult results = null;
+		AbnSearchResult results = null;
 
 		String params = "";
 
@@ -98,11 +98,11 @@ public class AbnSearchWSHttpGet
 		return results;
 	}
 
-	public static ditr.abn.example.AbnSearchResult searchByNameSimpleProtocol(String guid, String name, boolean legal, boolean trading, boolean act, boolean nsw,
+	public static AbnSearchResult searchByNameSimpleProtocol(String guid, String name, boolean legal, boolean trading, boolean act, boolean nsw,
 																			  boolean nt, boolean qld, boolean sa, boolean tas, boolean vic, boolean wa, String postcode) throws URISyntaxException, IOException,
 			SAXException, ParserConfigurationException, FactoryConfigurationError
 	{
-		ditr.abn.example.AbnSearchResult results = null;
+		AbnSearchResult results = null;
 
 		String params = "";
 
@@ -127,11 +127,11 @@ public class AbnSearchWSHttpGet
 		return results;
 	}
 
-	public static ditr.abn.example.AbnSearchResult searchByNameAdvancedSimpleProtocol(String guid, String name, boolean legal, boolean trading, boolean act,
+	public static AbnSearchResult searchByNameAdvancedSimpleProtocol(String guid, String name, boolean legal, boolean trading, boolean act,
 																					  boolean nsw, boolean nt, boolean qld, boolean sa, boolean tas, boolean vic, boolean wa, String postcode, String width, int minScore)
 			throws URISyntaxException, IOException, SAXException, ParserConfigurationException, FactoryConfigurationError
 	{
-		ditr.abn.example.AbnSearchResult results = null;
+		AbnSearchResult results = null;
 
 		String params = "";
 
@@ -160,10 +160,9 @@ public class AbnSearchWSHttpGet
 		return results;
 	}
 
-	private static ditr.abn.example.AbnSearchResult doRequest(String guid, String service, String parameters) throws URISyntaxException, IOException, SAXException,
-			ParserConfigurationException, FactoryConfigurationError
-	{
-		ditr.abn.example.AbnSearchResult result = null;
+	private static AbnSearchResult doRequest(String guid, String service, String parameters) throws URISyntaxException, IOException, SAXException,
+			ParserConfigurationException, FactoryConfigurationError {
+		AbnSearchResult result = null;
 
 		URL url = new URL("http://abr.business.gov.au/abrxmlsearch/ABRXMLSearch.asmx/" + service + "?authenticationGuid=" + URLEncoder.encode(guid, UTF_8) + parameters);
 
@@ -174,7 +173,7 @@ public class AbnSearchWSHttpGet
 		connection.connect();
 
 		if (connection.getResponseCode() == HttpURLConnection.HTTP_OK)
-			result = new ditr.abn.example.AbnSearchResult(XMLUtils.DOMParseXML(connection.getInputStream()).getDocumentElement());
+			result = new AbnSearchResult(XMLUtils.DOMParseXML(connection.getInputStream()).getDocumentElement());
 
 		connection.disconnect();
 
